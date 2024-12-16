f049eab62a8ea2aec983d51b8d20c587e35172c9
drvc3010 <46451311+drvc3010@users.noreply.github.com>
2024-12-15 20:47:16 -0500

    docker file

--- CurrencyConversionRepository/obj/Debug/net8.0/CurrencyRepository.AssemblyInfo.cs
+++ CurrencyConversionRepository/obj/Debug/net8.0/CurrencyRepository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyRepository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02a230938c96d5fb6e472781dc37e5b2bcb0f48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbf54a4ea459255e7c9a627877478ebf3209a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyRepository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
